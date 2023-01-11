2c5888c44d359972e14e03a1420e2b46d1b89072
alfie-mills <alfie.mills@outlook.com>
2023-01-11 18:21:47 +0000

    Rename dir and remove legacy files

--- workflow_automation/main.py
+++ workflow_automation/main.py
@@ -1,16 +1,8 @@
 # Importing the necessary modules
-import argparse
 import click
 from workflow_automation.backup import *
 from workflow_automation.cache import cache
 
-# #  Parse arguments
-# parser = argparse.ArgumentParser()
-# parser.add_argument("-b", "--backup")
-# parser.add_argument("-p", "--production")
-# parser.add_argument("-s", "--staging")
-# parser.add_argument("-d", "--development")
-# args = parser.parse_args()
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.group(context_settings=CONTEXT_SETTINGS)
 def cli():
